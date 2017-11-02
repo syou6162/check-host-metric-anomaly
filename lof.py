87574a5e902398c66746ed8dffba569293a13b96
Yasuhisa Yoshida <syou6162@gmail.com>
2017-11-03 04:00:04 +0900

    リファクタリング

--- lof.py
+++ lof.py
@@ -28,25 +28,23 @@ def get_lof(train, n_neighbors=20, contamination=0.01):
     return lof
  
 
-def get_predict(lof, test):
-    return lof._predict(test)
-
-
-def plot_result(train, test, test_average, window_size=20, contamination=0.01, n_neighbors=20):
+def get_predictions(train, test, contamination=0.01, n_neighbors=20):
     # 学習データから怪しいものを抜く
     # lof = LocalOutlierFactor(n_neighbors=n_neighbors, contamination=0.01)
     # train = np.delete(np.array(train), np.where(lof.fit_predict(np.array(train)) == -1)[0], axis=0)
 
     lof = get_lof(train, contamination=contamination, n_neighbors=n_neighbors)
-    result = get_predict(lof, test)
+    result = lof._predict(test)
     print(result[-10:])
+    return result
 
+
+def plot_result(filename, test_average, result, window_size):
     plt.plot(test_average)
     alert_idx = list(map(lambda n: n + window_size - 1, np.where(result == -1)[0]))
     plt.scatter(alert_idx, pd.Series(test_average)[alert_idx], color="red")
-    plt.savefig("/tmp/" + str(contamination) + "hoge.png")
+    plt.savefig("/tmp/" + filename)
     plt.close()
-    return result[-1]
 
 
 def main(args):
@@ -64,14 +62,17 @@ def main(args):
 
     print("NUM_TRAIN: " + str(len(train)))
 
-    warning_result = plot_result(train, test, test_average,
-                                 window_size=window_size, contamination=warning, n_neighbors=n_neighbors)
-    critical_result = plot_result(train, test, test_average,
-                                  window_size=window_size, contamination=critical, n_neighbors=n_neighbors)
+    warning_results = get_predictions(train, test, contamination=warning, n_neighbors=n_neighbors)
+    critical_results = get_predictions(train, test, contamination=critical, n_neighbors=n_neighbors)
 
-    if critical_result == -1:
+    warning_result_filename = "result_warning_" + str(warning) + ".png"
+    critical_result_filename = "result_critical_" + str(critical) + ".png"
+    plot_result(warning_result_filename, test_average, warning_results, window_size)
+    plot_result(critical_result_filename, test_average, critical_results, window_size)
+
+    if critical_results[-1] == -1:
         sys.exit(2)
-    elif warning_result == -1:
+    elif warning_results[-1] == -1:
         sys.exit(1)
     else:
         sys.exit(0)
